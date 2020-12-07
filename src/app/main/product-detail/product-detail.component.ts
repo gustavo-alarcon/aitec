cd16e2f038eefa1c332a34807646502d10fa4781
Melanie <mocharan@meraki-s.com>
2020-12-07 12:41:35 -0500

    merge conflicts

--- src/app/main/product-detail/product-detail.component.ts
+++ src/app/main/product-detail/product-detail.component.ts
@@ -57,13 +57,10 @@ export class ProductDetailComponent implements OnInit {
     private router: Router
   ) {
     this.route.paramMap.subscribe(params => {
-<<<<<<< Updated upstream
-=======
       console.log(params.get('param'));
 
       console.log(params['id']);
       
->>>>>>> Stashed changes
       
       this.ngOnInit();
     });
